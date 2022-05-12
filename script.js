d308dca0351097b00acaaa52a6237a31dce4905e
DahmanCode <elaammariabderrahman@gmail.com>
2022-05-12 23:53:50 +0100

    Add some improvements to menu bar

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
 document.querySelector('#search-icon').onclick = () => {
     document.querySelector('#search-form').classList.toggle('active');
 }
